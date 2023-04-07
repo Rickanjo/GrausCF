18d1edbacb862fd01904c05b6f083974765be6bc
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-07 11:34:35 -0300

    Exercício Resolvido

--- Program.cs
+++ Program.cs
@@ -5,9 +5,9 @@ double tempC;
 
 Console.WriteLine("---Conversor de °F para °C\n");
 
-Console.Write("Digite a temperatura em °F: ");
-tempF = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Digite a temperatura em °C: ");
+tempC = Convert.ToDouble(Console.ReadLine()!);
 
-tempC = tempF * 1.8 + 32;
+tempF = tempC * 1.8 + 32;
 
-Console.WriteLine($"\n{tempF}°F equivalem a {tempC}°C");
+Console.WriteLine($"\n{tempC}°C equivalem a {tempF}°F");
