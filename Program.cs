59f7e02700b6307b972be875c9bda74ec453da19
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-07 11:26:28 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//Converta uma temperatura digitada pelo usuário em °F para °C.
+
+double tempF;
+double tempC;
+
+Console.WriteLine("---Conversor de °F para °C\n");
+
+Console.Write("Digite a temperatura em °F: ");
+tempF = Convert.ToDouble(Console.ReadLine()!);
+
+tempC = tempF * 1.8 + 32;
+
+Console.WriteLine($"\n{tempF}°F equivalem a {tempC}°C");
